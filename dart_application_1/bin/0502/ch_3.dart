d6d62e6344d2e3da21dd7fe0f9863f46b8b5012b
Chin tsao han <cch3001@hotmail.com>
2024-05-09 20:47:45 +0800

    0509 at home

--- dart_application_1/bin/0502/ch_3.dart
+++ dart_application_1/bin/0502/ch_3.dart
@@ -1,15 +1,22 @@
 void main(List<String> args) {
-  print(myWidget.timeStamp!()) ;
-  print(myWidget2.timeStamp!()) ;
+  print(myWidget.timeStamp!());
+  print(myWidget2.timeStamp!());
 
-  int a=0;
-  void test(){
-    int a=10 ;//作用在 test function 區域內
-  for(int i=0; i< 10;i++){
-    print(i);
-    a=i;
+  int a = 0;
+  void test() {
+    int a = 10; //作用在 test function 區域內
+    for (int i = 0; i < 10; i++) {
+      print(i);
+      a = i;
+    }
+
+    Object d = 12;
+    // d.switch (expression) {
+    //   pattern => value,
+    // }
+    // print('${d.toString()}');
   }
-  }
+
   test();
   print('a $a');
 
@@ -19,17 +26,14 @@ void main(List<String> args) {
   // print(i);
 }
 
-class AnotherWidget{
-
+class AnotherWidget {
   final String Function()? timeStamp;
   //       為何加{}
   AnotherWidget({this.timeStamp});
-
 }
 
-final myWidget = AnotherWidget(timeStamp:() => DateTime.now().toIso8601String());
+final myWidget =
+    AnotherWidget(timeStamp: () => DateTime.now().toIso8601String());
 final timeStamp = myWidget.timeStamp?.call();
 
-final myWidget2 =  AnotherWidget(timeStamp: ()=>'123');
-
-
+final myWidget2 = AnotherWidget(timeStamp: () => '123');
